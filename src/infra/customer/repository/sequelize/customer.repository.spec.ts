6b4eaabd05418ab533b7422faae2cee579782837
marielifm <marieli.matos@hotmail.com>
2023-12-04 18:50:40 -0300

    refact: adding infra modules

--- src/infra/customer/repository/sequelize/customer.repository.spec.ts
+++ src/infra/customer/repository/sequelize/customer.repository.spec.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize-typescript';
 import CustomerRepository from './customer.repository';
-import Customer from '../../domain/customer/entity/customer';
-import Address from '../../domain/customer/value-object/address';
-import CustomerModel from '../db/sequelize/model/customer.model';
+import Customer from '../../../../domain/customer/entity/customer';
+import Address from '../../../../domain/customer/value-object/address';
+import CustomerModel from './customer.model';
 
 describe('Customer repository test', () => {
   let sequelize: Sequelize;
--- src/infra/order/repository/sequilize/order.repository.ts
+++ src/infra/order/repository/sequilize/order.repository.ts
@@ -1,9 +1,9 @@
-import Customer from '../../domain/customer/entity/customer';
-import Order from '../../domain/checkout/entity/order';
-import OrderRepositoryInterface from '../../domain/checkout/repository/order-repository.interface';
-import OrderItemModel from '../db/sequelize/model/order-item.model';
-import OrderModel from '../db/sequelize/model/order.model';
-import OrderItem from '../../domain/checkout/entity/orderItem';
+import Customer from '../../../../domain/customer/entity/customer';
+import Order from '../../../../domain/checkout/entity/order';
+import OrderRepositoryInterface from '../../../../domain/checkout/repository/order-repository.interface';
+import OrderItemModel from './order-item.model';
+import OrderModel from './order.model';
+import OrderItem from '../../../../domain/checkout/entity/orderItem';
 
 export default class OrderRepository implements OrderRepositoryInterface {
   async update(entity: Order): Promise<void> {
